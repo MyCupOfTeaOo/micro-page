f7359d7726328bb21ccc96c6821dcdbad269a20c
zou wendi <542095059@qq.com>
2020-05-19 16:22:52 +0800

    修复svgbug

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,7 +1,17 @@
 import { IBundleOptions } from 'father';
 
+const fs = require('fs');
+
+let babelPlugin = [];
+
+if (fs.existsSync('./.babelrc')) {
+  const babelJson = JSON.parse(fs.readFileSync('./.babelrc').toString());
+  babelPlugin = babelPlugin.concat(babelJson.plugins || []);
+}
+
 const options: IBundleOptions = {
   esm: 'babel',
+  extraBabelPlugins: babelPlugin,
   pkgs: ['micro-page-core', 'micro-page-react', 'micro-page-plugin-preset'],
 };
 
